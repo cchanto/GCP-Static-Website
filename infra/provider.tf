ce651620cc178f68d31b39148fa88e744cdcce22
Carlos Chanto <35463225+cchanto@users.noreply.github.com>
2024-10-29 12:02:52 -0600

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -2,14 +2,14 @@
 
 provider "google" {
   #credentials  = file(var.gcp_svc_key)
-  project      = var.gcp_project
+  project      = var.project_id
   region       = var.gcp_region
 }
 
 # GCP beta provider
 provider "google-beta" {
   #credentials  = file(var.gcp_svc_key)
-  project      = var.gcp_project
+  project      = var.project_id
   region       = var.gcp_region
 }
 
